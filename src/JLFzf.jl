5a670bb1eacb29815fa46d4719afb3bc97317437
Denis Telnov <denis.telnov@gmail.com>
2020-08-25 22:21:21 +0300

    Improve `read_repl_hist()` and fix running `fzf`

--- src/JLFzf.jl
+++ src/JLFzf.jl
@@ -2,38 +2,51 @@ module JLFzf
 
 import fzf_jll
 using Pipe
+import REPL
 
-export inter_fzf
+export inter_fzf, read_repl_hist
 
+"""
+    read_repl_hist()
+
+Find corresponding history file, read it, split by history records
+and return resulted `Vector{String}` in reverse order.
+"""
 function read_repl_hist()
-    @pipe open("~/.julia/logs/repl_history.jl" |> expanduser) |> read |> String |> 
-    replace(_, "\t" => "") |> #remove tabs
-    split(_, "\n") |> #split by new lines
-    filter(x->!occursin("#", x), _) |> #remove comments lines end
-    unique |> reverse # keep unique entries
+    rx = r"(^|\n)# time:[^\n]*\n# mode:[^\n]*\n"m
+    @pipe open(REPL.find_hist_file()) |>
+    read |> String |>
+    replace(_, r"\n$" => "") |>           # remove last new line
+    replace(_, r"^\t"m => "") |>          # remove leading tabs
+    split(_, rx, keepempty = false) |>    # split by history records
+    reverse |> unique                     # keep unique entries
 end
 
 """
-inter_fzf(in_str::String)
-run interactive fzf and return selected result, `in_str` should contain `\n`s,
-return selected string
+    inter_fzf(in_str::String, args...)
+
+Run interactive fzf and return selected result, by default `in_str` should use `\n`s as the delimiter,
+return selected string.
+Additional arguments `args` for `fzf` are allowed.
 """
 function inter_fzf(in_str::String, args...)
     fzf_jll.fzf() do exe
         if length(args) == 0
-            return read(pipeline(`$exe`, stdin = IOBuffer(in_str)), String) |> chomp
+            return read(pipeline(Cmd(`$exe`, ignorestatus = true), stdin = IOBuffer(in_str)), String) |> chomp
         else
-            return read(pipeline(`$exe $(join(args, ' '))`, stdin = IOBuffer(in_str)), String) |> chomp
+            return read(pipeline(Cmd(`$exe $(args)`, ignorestatus = true), stdin = IOBuffer(in_str)), String) |> chomp
         end
     end
 end
 
 """
-inter_fzf(ary::AbstractArray)
-run interactive fzf with an array of inputs, return selected string
+    inter_fzf(ary::AbstractArray, args...)
+
+Run interactive fzf with an array of inputs, return selected string.
+Additional arguments `args` for `fzf` are allowed.
 """
 function inter_fzf(ary::AbstractArray, args...)
-    inter_fzf(join(ary, "\n"), args...)
+    inter_fzf(join(ary, '\0'), args...)
 end
 
 end
