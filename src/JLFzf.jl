7c93863022ebbbd6393ebfac6c2502236dcdef14
Denis Telnov <denis.telnov@gmail.com>
2020-08-26 01:31:36 +0300

    Add switching to REPL modes stored in history

--- src/JLFzf.jl
+++ src/JLFzf.jl
@@ -3,8 +3,9 @@ module JLFzf
 import fzf_jll
 using Pipe
 import REPL
+import REPL.LineEdit
 
-export inter_fzf, read_repl_hist
+export inter_fzf, read_repl_hist, insert_history_to_repl
 
 """
     read_repl_hist()
@@ -13,11 +14,20 @@ Find corresponding history file, read it, split by history records
 and return resulted `Vector{String}` in reverse order.
 """
 function read_repl_hist()
-    rx = r"(^|\n)# time:[^\n]*\n# mode:[^\n]*\n"m
+    rx  = r"(^|\n)# time:[^\n]*\n"m
+    # markers for history modes, used by `insert_history_to_repl()`
+    rxj = r"^# mode: julia\n"m => ""
+    rxh = r"^# mode: help\n"m  => "?\n"
+    rxp = r"^# mode: pkg\n"m   => "]\n"
+    rxs = r"^# mode: shell\n"m => ";\n"
     @pipe open(REPL.find_hist_file()) |>
     read |> String |>
     replace(_, r"\n$" => "") |>           # remove last new line
     replace(_, r"^\t"m => "") |>          # remove leading tabs
+    replace(_, rxj) |>                    # replace mode with corresponding marker
+    replace(_, rxh) |>                    #
+    replace(_, rxp) |>                    #
+    replace(_, rxs) |>                    #
     split(_, rx, keepempty = false) |>    # split by history records
     reverse |> unique                     # keep unique entries
 end
@@ -47,6 +57,35 @@ Additional arguments `args` for `fzf` are allowed.
 """
 function inter_fzf(ary::AbstractArray, args...)
     inter_fzf(join(ary, '\0'), args...)
+end
+
+function edit_insert_and_state_transition(mistate, line, mode)
+    LineEdit.edit_insert(mistate, line)
+    iobuffer = LineEdit.buffer(mistate)
+    LineEdit.transition(mistate, Base.active_repl.interface.modes[mode]) do
+        prompt_state = LineEdit.state(mistate, Base.active_repl.interface.modes[mode])
+        prompt_state.input_buffer = copy(iobuffer)
+    end
+end
+
+"""
+    insert_history_to_repl(mistate, line)
+
+Paste found by `fzf` history `line` into REPL `mistate`.
+"""
+function insert_history_to_repl(mistate, line)
+    rxh = r"^\?\n"
+    rxp = r"^\]\n"
+    rxs = r"^;\n"
+    if occursin(rxh, line)
+        edit_insert_and_state_transition(mistate, replace(line, rxh => ""), 3)
+    elseif occursin(rxp, line)
+        edit_insert_and_state_transition(mistate, replace(line, rxp => ""), 6)
+    elseif occursin(rxs, line)
+        edit_insert_and_state_transition(mistate, replace(line, rxs => ""), 2)
+    else
+        LineEdit.edit_insert(mistate, line)
+    end
 end
 
 end
